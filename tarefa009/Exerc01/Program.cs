44cfc2b112135e99b155842c227395be8f12aece
samirmont0 <samirmont0@hotmail.com>
2021-08-20 17:23:02 -0300

    Tarefa 009 como foi pedido pelo professor

---
+++ tarefa009/Exerc01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exerc1
+{
+    class Program
+    {
+        public static int IdadeEmDias(string anosIdade, string mesesIdade, string diasIdade)
+        {
+            int totalDias = int.Parse(anosIdade) * 365 + int.Parse(mesesIdade) * 30 + int.Parse(diasIdade);
+            return totalDias;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] idade;
+            Console.WriteLine("Digite sua idade: ");
+            idade = Console.ReadLine().Split(" ");
+            Console.WriteLine("O total de dias correspondente à sua idade é: " + IdadeEmDias(idade[0], idade[1], idade[2]) + "dias");
+        }
+    }
+}
---
+++ tarefa009/Exerc03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exerc3
+{
+    class Program
+    {
+        public static double saldoReajustado(double saldo)
+        {
+            double saldoComReajuste = saldo * 1.15;
+            return saldoComReajuste;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o saldo: ");
+            double _saldo = double.Parse(Console.ReadLine());
+            Console.WriteLine("O saldo reajustado em 15%, ficou " + saldoReajustado(_saldo) + " reais");
+        }
+    }
+}
