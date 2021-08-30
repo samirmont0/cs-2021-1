f48a626e4f5f031c43a6e4f45e4e9638ba8c29dd
samirmont0 <samirmont0@hotmail.com>
2021-08-29 22:55:27 -0300

    Adiciona tarefa 013

---
+++ tarefa013/AnimalTeste.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tarefa13_1
+{
+    class AnimalTeste
+    {
+        static void Main(string[] args)
+        {
+            Cachorro cachorro = new Cachorro();
+            cachorro.Nome = "Boltz";
+            cachorro.Idade = 3;
+
+            Cavalo cavalo = new Cavalo();
+            cavalo.Nome = "Brabo";
+            cavalo.Idade = 1;
+
+            Preguica preguica = new Preguica();
+            preguica.Nome = "Lentois";
+            preguica.Idade = 1;
+
+            List<Animal> animais = new List<Animal>();
+            animais.Add(cachorro);
+            animais.Add(cavalo);
+            animais.Add(preguica);
+
+            foreach(var animal in animais)
+            {
+                Console.WriteLine(animal.EmitirSom());
+            }
+
+            Veterinario vet = new Veterinario();
+            vet.Examinar(cachorro);
+            vet.Examinar(cavalo);
+            vet.Examinar(preguica);
+        }
+    }
+}
---
+++ tarefa013/Veterinario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarefa13_1
+{
+    class Veterinario
+    {
+        public void Examinar(Animal animal)
+        {
+            Console.WriteLine(animal.EmitirSom());
+        }
+    }
+}
